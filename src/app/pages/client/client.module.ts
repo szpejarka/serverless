51fd7333c51fd328f727e7f8ec09a89f688bad2f
jacek <jacek@DESKTOP-B9AE4Q3>
2018-03-13 23:30:07 +0100

    o

--- src/app/pages/client/client.module.ts
+++ src/app/pages/client/client.module.ts
@@ -8,17 +8,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ClientListItemComponent } from './list/client-list-item-component';
 import { ClientDetailComponent } from './detail/client-detail-component';
 import { AuthGuard } from '../auth-guard-service';
+import { ClientFeatureComponent } from './client-feature/client-feature.component';
 
 const clientRoutes: Routes = [
-  { path: 'client/:id', component: ClientDetailComponent, canActivate: [AuthGuard]},
-  { path: 'client',  component: ClientListComponent, canActivate: [AuthGuard]}
-  ];
+  { path: 'client', component: ClientFeatureComponent, children: [
+    { path: '',  component: ClientListComponent,/* canActivate: [AuthGuard]*/ children: [
+      { path: ':id', component: ClientDetailComponent, /*canActivate: [AuthGuard]*/}
+    ]}
+  ]}
+];
 
 @NgModule({
     declarations: [
       ClientListComponent,
       ClientListItemComponent,
-      ClientDetailComponent
+      ClientDetailComponent,
+      ClientFeatureComponent
     ],
     imports: [
       BrowserModule,
